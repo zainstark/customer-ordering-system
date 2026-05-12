d094c203585665e64477db1f880c15f63b770fc8
Aki3006 <amr.120230092@ejust.edu.eg>
2026-05-12 17:10:23 +0300

    Added api client + injector

---
+++ frontend/lib/Core/injector/injector.dart
@@ -0,0 +1,11 @@
+import 'package:frontend/Core/network/dio_client.dart';
+import 'package:get_it/get_it.dart';
+
+final getIt = GetIt.instance;
+
+void setupDependencies() {
+  // Register DioClient as a singleton
+  getIt.registerSingleton<DioClient>(
+    DioClient(),
+  );
+}
---
+++ frontend/lib/Core/network/dio_client.dart
@@ -0,0 +1,78 @@
+import 'package:dio/dio.dart';
+
+class DioClient {
+  final Dio _dio;
+
+  DioClient()
+      : _dio = Dio(BaseOptions(
+        // TODO: Set the base URL for your API here
+          baseUrl: "",
+          connectTimeout: const Duration(seconds: 10),
+          receiveTimeout: const Duration(seconds: 10),
+          headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+          },
+        )) {
+    _setupInterceptors();
+  }
+
+  void _setupInterceptors() {
+    _dio.interceptors.add(
+      InterceptorsWrapper(
+        onRequest: (options, handler) {
+          // Add any request modifications here (e.g., auth token)
+          return handler.next(options);
+        },
+        onResponse: (response, handler) {
+          return handler.next(response);
+        },
+        onError: (error, handler) {
+          return handler.next(error);
+        },
+      ),
+    );
+  }
+
+  // GET request
+  Future<Response> get(
+    String path, {
+    Map<String, dynamic>? queryParameters,
+    Options? options,
+  }) async {
+    return _dio.get(path, queryParameters: queryParameters, options: options);
+  }
+
+  // POST request
+  Future<Response> post(
+    String path, {
+    dynamic data,
+    Map<String, dynamic>? queryParameters,
+    Options? options,
+  }) async {
+    return _dio.post(path,
+        data: data, queryParameters: queryParameters, options: options);
+  }
+
+  // PUT request
+  Future<Response> put(
+    String path, {
+    dynamic data,
+    Map<String, dynamic>? queryParameters,
+    Options? options,
+  }) async {
+    return _dio.put(path,
+        data: data, queryParameters: queryParameters, options: options);
+  }
+
+  // DELETE request
+  Future<Response> delete(
+    String path, {
+    dynamic data,
+    Map<String, dynamic>? queryParameters,
+    Options? options,
+  }) async {
+    return _dio.delete(path,
+        data: data, queryParameters: queryParameters, options: options);
+  }
+}
