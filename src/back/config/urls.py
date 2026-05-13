e00e2b0abda858a405dced967968bee768714854
Ibrahim Hafez <ibrahimyaser2004@gmail.com>
2026-05-13 03:20:09 +0300

    Add apps to INSTALLED_APPS and move into src/back

--- src/back/config/urls.py
+++ src/back/config/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for config project.
+URL configuration for order_service project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/6.0/topics/http/urls/
@@ -14,10 +14,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path('admin/', admin.site.urls),
 ]
