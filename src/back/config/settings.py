21ac2389921850240466f0fb8cf3bfefb723f7d6
AhmedElshentenawy <elshentenawya@gmail.com>
2026-05-17 13:46:21 +0300

    Fixing URLs

--- src/back/config/settings.py
+++ src/back/config/settings.py
@@ -16,6 +16,9 @@ from datetime import timedelta
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
