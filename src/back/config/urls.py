f613970996ace637cad6a1de40f49c1073f676e6
Ibrahim Hafez <ibrahimyaser2004@gmail.com>
2026-05-17 07:13:23 +0300

    Add url for order

--- src/back/config/urls.py
+++ src/back/config/urls.py
@@ -24,5 +24,5 @@ urlpatterns = [
     path('api/auth/', include('rest_framework.urls')),
     path('api/cart/', include('apps.cart.urls')),
     path("menu/", include("apps.menu.urls")),
-    path('order/', include('apps.order.urls')),
+    path('api/order/', include('apps.order.urls')),
 ]
